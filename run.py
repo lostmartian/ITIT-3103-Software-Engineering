b830ec7d98432118ed86d0192ae0b21e4072a7be
lostmartian <sahilgangurde08@gmail.com>
2021-11-07 22:14:42 +0530

    Backend viewBoard function added

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, render_template, g, current_app
 from database import queryDatabase, importDatabase
-from board import cb
+from board import cb, individualBoardPostData
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    board = queryDatabase('SELECT * FROM board')
+    board = queryDatabase('SELECT * FROM board ORDER BY board_name')
     board1, board2, board3 = [], [], []
     sz = len(board)
     for i in range(0, sz):
         if i < sz//3:
             board1.append(board[i])
-        elif i > sz//3 and i < (2*(sz//3)):
+        elif i >= sz//3 and i < (2*(sz//3)):
             board2.append(board[i])
         else:
             board3.append(board[i])
@@ -21,6 +21,10 @@ def index():
 @app.route('/createBoard/', methods = ['GET', 'POST'])
 def createBoard():
     return cb()
+
+@app.route('/b/<board>')
+def showBoard(board):
+    return individualBoardPostData
 
 if __name__== "__main__":
     app.run(debug=True)
