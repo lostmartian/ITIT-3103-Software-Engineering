88ec58278c05823eee29cc23d3fca4793a0c75fa
lostmartian <sahilgangurde08@gmail.com>
2021-11-15 14:35:34 +0530

    added boards and redirects fixed

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def createBoard():
 
 @app.route('/b/<board>')
 def showBoard(board):
-    return individualBoardPostData
+    return individualBoardPostData(board)
 
 if __name__== "__main__":
     app.run(debug=True)
